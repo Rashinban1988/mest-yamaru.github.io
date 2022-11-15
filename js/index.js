f24f21d2543b16719e5823908e3bcb242494a4db
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-11-16 08:09:40 +0900

    レスポンシブ調整、ハンバーガーメニュー調整

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(function(){
 
+    // // Topページのみ遅れてヘッダー出現
+    // if($('body').hasClass('home blog')) {
+    //     // Topページだったらヘッダーアニメーション
+    //     topTextAnimation('.hrn-animation',1000,3800);
+    // } else { // 違ったらそのまま表示
+    //     $('.hrn-animation').css('opacity',1);
+    // }
+
     let mainCoverEl = document.getElementById("main-cover")
 
     $('#jaticker-1').jaticker({'autoStart': false});
@@ -18,18 +26,36 @@ $(function(){
     /*============================
     |    ハンバーガーメニュー    |
     =============================*/
-    // クリックでメニュー表示
-    $('.hb-menu-area').click(function() {
-        $('.hb-menu-area span').toggleClass('op-line');
-        $('.sp-op-msk').toggleClass('op-msk');
-        $('.sp-op-menu').toggleClass('op-menu');
+
+    $(".btn-gnavi").on("click", function(){
+        // ハンバーガーメニューの位置を設定
+        var rightVal = 0;
+        if($(this).hasClass("open")) {
+            // 位置を移動させメニューを開いた状態にする
+            rightVal = -300;
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).removeClass("open");
+        } else {
+            // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+            $(this).addClass("open");
+        }
+        $("#global-navi").stop().animate({
+            right: rightVal
+        }, 200);
     });
 
-    // メニューまたは背景タップで戻る
-    $('.hb-cls').click(function() {
-        $('.hb-menu-area span').removeClass('op-line');
-        $('.sp-op-msk').removeClass('op-msk');
-        $('.sp-op-menu').removeClass('op-menu');
-    });
+    // // クリックでメニュー表示
+    // $('.hb-menu-area').click(function() {
+    //     $('.hb-menu-area span').toggleClass('op-line');
+    //     $('.sp-op-msk').toggleClass('op-msk');
+    //     $('.sp-op-menu').toggleClass('op-menu');
+    // });
+
+    // // メニューまたは背景タップで戻る
+    // $('.hb-cls').click(function() {
+    //     $('.hb-menu-area span').removeClass('op-line');
+    //     $('.sp-op-msk').removeClass('op-msk');
+    //     $('.sp-op-menu').removeClass('op-menu');
+    // });
 
 });
